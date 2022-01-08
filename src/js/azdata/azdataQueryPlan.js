0eef6869d1e7b6e216be839370fec23fc656f6c7
Lewis Sanchez <lewissanchez@microsoft.com>
2022-01-07 17:29:34 -0800

    Adds new method to add inverted edge to graph

--- src/js/azdata/azdataQueryPlan.js
+++ src/js/azdata/azdataQueryPlan.js
@@ -124,7 +124,7 @@ azdataQueryPlan.prototype.init = function(container, iconPaths)
                         iconName = 'azdataQueryplan-' +  icons[rand];
                     }
                     vertex = graph.insertVertex(parent, null, node, 20, 20, 70, 70, iconName);
-                    graph.insertEdge(parent, null, '', entry.vertex, vertex);
+                    graph.insertInvertedEdge(parent, null, '', entry.vertex, vertex);
                     stack.push(
                         { 
                             vertex: vertex,
