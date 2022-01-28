1c3a6ffdc59207de102616234096430aae9addc3
Lewis Sanchez <87730006+lewis-sanchez@users.noreply.github.com>
2022-01-27 16:31:33 -0800

    Adds zooming Functionality to azdataGraph (#8)
    * Adds zooming functionality to graph
    
    * renames zoom out and zoom in button click handler
    
    * Minor code clean up
    
    * Removes embedded zoom buttons, adds zoom listeners, and adds test file
    
    * Generalizes zoom listener, so zooming can be triggered by any action

--- src/js/azdata/azdataQueryPlan.js
+++ src/js/azdata/azdataQueryPlan.js
@@ -1,48 +1,50 @@
-function azdataQueryPlan(container, queryPlanGraph, iconPaths)
-{
+function azdataQueryPlan(container, queryPlanGraph, iconPaths) {
     this.queryPlanGraph = queryPlanGraph;
-	if (container != null && iconPaths != null)
-	{
-		this.init(container, iconPaths);
-	}
+    if (container != null && iconPaths != null) {
+        this.init(container, iconPaths);
+    }
 };
 
-azdataQueryPlan.prototype.init = function(container, iconPaths)
-{
+azdataQueryPlan.prototype.init = function (container, iconPaths) {
+    let div = document.getElementById('graphContainer');
+    div.style.overflow = 'auto';
     this.container = container;
 
-    mxEvent.addListener(window, 'unload', mxUtils.bind(this, function()
-    {
+    mxEvent.addListener(window, 'unload', mxUtils.bind(this, function () {
         this.destroy();
     }));
 
     mxEvent.disableContextMenu(container);
 
     var graph = new azdataGraph(container);
-    graph.setPanning(true);
-	graph.setTooltips(true);        
+    this.graph = graph;
+    graph.centerZoom = false;
+    graph.setTooltips(true);
+    graph.setEnabled(true);
 
-    graph.convertValueToString = function(cell)
-    {
-        if (cell.value != null && cell.value.label != null)
-        {
+    graph.panningHandler.useLeftButtonForPanning = true;
+    graph.setPanning(true);
+    graph.resizeContainer = false;
+
+    graph.convertValueToString = function (cell) {
+        if (cell.value != null && cell.value.label != null) {
             return cell.value.label;
         }
 
         return azdataGraph.prototype.convertValueToString.apply(this, arguments); // "supercall"
     };
 
-    graph.isHtmlLabel = function(cell)
-    {
-        return false;
-    };
-    
-    graph.isCellEditable = function(cell)
-    {
+    graph.isHtmlLabel = function (cell) {
         return false;
     };
 
-    graph.addListener(mxEvent.CLICK, azdataGraph.prototype.graphClickEventHandler);
+    graph.isCellEditable = function (cell) {
+        return false;
+    };
+
+    graph.addListener(mxEvent.CLICK, (sender, event) => {
+        azdataGraph.prototype.graphClickEventHandler(sender, event);
+    });
 
     graph.getTooltipForCell = azdataGraph.prototype.getStyledTooltipForCell;
 
@@ -53,7 +55,7 @@ azdataQueryPlan.prototype.init = function(container, iconPaths)
     style = mxUtils.clone(style);
     style[mxConstants.STYLE_SHAPE] = mxConstants.SHAPE_LABEL;
     style[mxConstants.STYLE_STROKECOLOR] = '#ffffff';
-	style[mxConstants.STYLE_FILLCOLOR] = '#ffffff';
+    style[mxConstants.STYLE_FILLCOLOR] = '#ffffff';
     style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_CENTER;
     style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_TOP;
     style[mxConstants.STYLE_IMAGE_ALIGN] = mxConstants.ALIGN_CENTER;
@@ -64,8 +66,7 @@ azdataQueryPlan.prototype.init = function(container, iconPaths)
     style[mxConstants.STYLE_SPACING] = '8';
 
     var icons = new Array();
-    for (const iconName in iconPaths) 
-    {
+    for (const iconName in iconPaths) {
         console.log(iconName + ' - ' + iconPaths[iconName]);
         style = mxUtils.clone(style);
         style[mxConstants.STYLE_IMAGE] = iconPaths[iconName];
@@ -74,43 +75,39 @@ azdataQueryPlan.prototype.init = function(container, iconPaths)
     }
 
     graph.getModel().beginUpdate();
-    try
-    {
+    try {
         var rand = Math.floor((Math.random() * icons.length));
 
         var iconName = undefined;
         if (this.queryPlanGraph.icon) {
             iconName = 'azdataQueryplan-' + this.queryPlanGraph.icon
         } else {
-            iconName = 'azdataQueryplan-' +  icons[rand];
+            iconName = 'azdataQueryplan-' + icons[rand];
         }
 
         var vertex = graph.insertVertex(parent, null, this.queryPlanGraph, 20, 20, 70, 70, iconName);
-        var stack = 
-        [
-            { 
-                vertex: vertex,
-                node: this.queryPlanGraph 
-            }
-        ];
-        while (stack.length > 0)
-        {
+        var stack =
+            [
+                {
+                    vertex: vertex,
+                    node: this.queryPlanGraph
+                }
+            ];
+        while (stack.length > 0) {
             var entry = stack.pop();
-            if (entry.node.children)
-            {
-                for (var i = 0; i < entry.node.children.length; ++i)
-                {                    
+            if (entry.node.children) {
+                for (var i = 0; i < entry.node.children.length; ++i) {
                     var node = entry.node.children[i];
                     if (node.icon) {
                         iconName = 'azdataQueryplan-' + node.icon
                     } else {
                         rand = Math.floor((Math.random() * icons.length));
-                        iconName = 'azdataQueryplan-' +  icons[rand];
+                        iconName = 'azdataQueryplan-' + icons[rand];
                     }
                     vertex = graph.insertVertex(parent, null, node, 20, 20, 70, 70, iconName);
 
                     let edgeInfo = {
-                        label:'',
+                        label: '',
                         metrics: [{
                             'name': `Estimated Number of Rows Per Execution`,
                             'value': `${Math.floor(Math.random() * 500)}`,
@@ -139,16 +136,21 @@ azdataQueryPlan.prototype.init = function(container, iconPaths)
         }
         layout.execute(parent);
     }
-    finally
-    {
+    finally {
         graph.getModel().endUpdate();
     }
 };
 
-azdataQueryPlan.prototype.destroy = function()
-{
-    if (!this.destroyed)
-	{
+azdataQueryPlan.prototype.registerZoomInListener = function (element, eventType) {
+    this.graph.addZoomInListener(element, eventType);
+};
+
+azdataQueryPlan.prototype.registerZoomOutListener = function (element, eventType) {
+    this.graph.addZoomOutListener(element, eventType);
+};
+
+azdataQueryPlan.prototype.destroy = function () {
+    if (!this.destroyed) {
         this.destroyed = true;
         this.container = null;
     }
