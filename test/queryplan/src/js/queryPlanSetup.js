eabc67f933f53728dce12d1f0cb0f2becb2df6d8
Lewis Sanchez <lewissanchez@microsoft.com>
2022-01-27 17:21:14 -0800

    Formats queryPlanSetup.js

--- test/queryplan/src/js/queryPlanSetup.js
+++ test/queryplan/src/js/queryPlanSetup.js
@@ -8,250 +8,250 @@ function getGraph() {
 
 function getIconPaths(imageBasePath) {
 
-    var iconPaths =
-		{
-			// generic icons
-			iteratorCatchAll: imageBasePath + 'iterator_catch_all.png',
+	var iconPaths =
+	{
+		// generic icons
+		iteratorCatchAll: imageBasePath + 'iterator_catch_all.png',
 
-			cursorCatchAll: imageBasePath + 'cursor_catch_all.png',
+		cursorCatchAll: imageBasePath + 'cursor_catch_all.png',
 
-			languageConstructCatchAll: imageBasePath + 'language_construct_catch_all.png',
+		languageConstructCatchAll: imageBasePath + 'language_construct_catch_all.png',
 
-			// operator icons
-			adaptiveJoin: imageBasePath + 'adaptive_join.png',
+		// operator icons
+		adaptiveJoin: imageBasePath + 'adaptive_join.png',
 
-			assert: imageBasePath + 'assert.png',
+		assert: imageBasePath + 'assert.png',
 
-			bitmap: imageBasePath + 'bitmap.png',
+		bitmap: imageBasePath + 'bitmap.png',
 
-			clusteredIndexDelete: imageBasePath + 'clustered_index_delete.png',
+		clusteredIndexDelete: imageBasePath + 'clustered_index_delete.png',
 
-			clusteredIndexInsert: imageBasePath + 'clustered_index_insert.png',
+		clusteredIndexInsert: imageBasePath + 'clustered_index_insert.png',
 
-			clusteredIndexScan: imageBasePath + 'clustered_index_scan.png',
+		clusteredIndexScan: imageBasePath + 'clustered_index_scan.png',
 
-			clusteredIndexSeek: imageBasePath + 'clustered_index_seek.png',
+		clusteredIndexSeek: imageBasePath + 'clustered_index_seek.png',
 
-			clusteredIndexUpdate: imageBasePath + 'clustered_index_update.png',
+		clusteredIndexUpdate: imageBasePath + 'clustered_index_update.png',
 
-			clusteredIndexMerge: imageBasePath + 'clustered_index_merge.png',
+		clusteredIndexMerge: imageBasePath + 'clustered_index_merge.png',
 
-			clusteredUpdate: imageBasePath + 'clustered_update.png',
+		clusteredUpdate: imageBasePath + 'clustered_update.png',
 
-			collapse: imageBasePath + 'collapse.png',
+		collapse: imageBasePath + 'collapse.png',
 
-			computeScalar: imageBasePath + 'compute_scalar.png',
+		computeScalar: imageBasePath + 'compute_scalar.png',
 
-			concatenation: imageBasePath + 'concatenation.png',
+		concatenation: imageBasePath + 'concatenation.png',
 
-			constantScan: imageBasePath + 'constant_scan.png',
+		constantScan: imageBasePath + 'constant_scan.png',
 
-			deletedScan: imageBasePath + 'deleted_scan.png',
+		deletedScan: imageBasePath + 'deleted_scan.png',
 
-			filter: imageBasePath + 'filter.png',
+		filter: imageBasePath + 'filter.png',
 
-			hashMatch: imageBasePath + 'hash_match.png',
+		hashMatch: imageBasePath + 'hash_match.png',
 
-			indexDelete: imageBasePath + 'index_delete.png',
+		indexDelete: imageBasePath + 'index_delete.png',
 
-			indexInsert: imageBasePath + 'index_insert.png',
+		indexInsert: imageBasePath + 'index_insert.png',
 
-			indexScan: imageBasePath + 'index_scan.png',
+		indexScan: imageBasePath + 'index_scan.png',
 
-			columnstoreIndexDelete: imageBasePath + 'columnstore_index_delete.png',
+		columnstoreIndexDelete: imageBasePath + 'columnstore_index_delete.png',
 
-			columnstoreIndexInsert: imageBasePath + 'columnstore_index_insert.png',
+		columnstoreIndexInsert: imageBasePath + 'columnstore_index_insert.png',
 
-			columnstoreIndexMerge: imageBasePath + 'columnstore_index_merge.png',
+		columnstoreIndexMerge: imageBasePath + 'columnstore_index_merge.png',
 
-			columnstoreIndexScan: imageBasePath + 'columnstore_index_scan.png',
+		columnstoreIndexScan: imageBasePath + 'columnstore_index_scan.png',
 
-			columnstoreIndexUpdate: imageBasePath + 'columnstore_index_update.png',
+		columnstoreIndexUpdate: imageBasePath + 'columnstore_index_update.png',
 
-			indexSeek: imageBasePath + 'index_seek.png',
+		indexSeek: imageBasePath + 'index_seek.png',
 
-			indexSpool: imageBasePath + 'index_spool.png',
+		indexSpool: imageBasePath + 'index_spool.png',
 
-			indexUpdate: imageBasePath + 'index_update.png',
+		indexUpdate: imageBasePath + 'index_update.png',
 
-			insertedScan: imageBasePath + 'inserted_scan.png',
+		insertedScan: imageBasePath + 'inserted_scan.png',
 
-			logRowScan: imageBasePath + 'log_row_scan.png',
+		logRowScan: imageBasePath + 'log_row_scan.png',
 
-			mergeInterval: imageBasePath + 'merge_interval.png',
+		mergeInterval: imageBasePath + 'merge_interval.png',
 
-			mergeJoin: imageBasePath + 'merge_join.png',
+		mergeJoin: imageBasePath + 'merge_join.png',
 
-			nestedLoops: imageBasePath + 'nested_loops.png',
+		nestedLoops: imageBasePath + 'nested_loops.png',
 
-			parallelism: imageBasePath + 'parallelism.png',
+		parallelism: imageBasePath + 'parallelism.png',
 
-			parameterTableScan: imageBasePath + 'parameter_table_scan.png',
+		parameterTableScan: imageBasePath + 'parameter_table_scan.png',
 
-			print: imageBasePath + 'print.png',
+		print: imageBasePath + 'print.png',
 
-			rank: imageBasePath + 'rank.png',
+		rank: imageBasePath + 'rank.png',
 
-			foreignKeyReferencesCheck: imageBasePath + 'foreign_key_references_check.png',
+		foreignKeyReferencesCheck: imageBasePath + 'foreign_key_references_check.png',
 
-			remoteDelete: imageBasePath + 'remote_delete.png',
+		remoteDelete: imageBasePath + 'remote_delete.png',
 
-			remoteIndexScan: imageBasePath + 'remote_index_scan.png',
+		remoteIndexScan: imageBasePath + 'remote_index_scan.png',
 
-			remoteIndexSeek: imageBasePath + 'remote_index_seek.png',
+		remoteIndexSeek: imageBasePath + 'remote_index_seek.png',
 
-			remoteInsert: imageBasePath + 'remote_insert.png',
+		remoteInsert: imageBasePath + 'remote_insert.png',
 
-			remoteQuery: imageBasePath + 'remote_query.png',
+		remoteQuery: imageBasePath + 'remote_query.png',
 
-			remoteScan: imageBasePath + 'remote_scan.png',
+		remoteScan: imageBasePath + 'remote_scan.png',
 
-			remoteUpdate: imageBasePath + 'remote_update.png',
+		remoteUpdate: imageBasePath + 'remote_update.png',
 
-			ridLookup: imageBasePath + 'rid_lookup.png',
+		ridLookup: imageBasePath + 'rid_lookup.png',
 
-			rowCountSpool: imageBasePath + 'row_count_spool.png',
+		rowCountSpool: imageBasePath + 'row_count_spool.png',
 
-			segment: imageBasePath + 'segment.png',
+		segment: imageBasePath + 'segment.png',
 
-			sequence: imageBasePath + 'sequence.png',
+		sequence: imageBasePath + 'sequence.png',
 
-			sequenceProject: imageBasePath + 'sequence_project.png',
+		sequenceProject: imageBasePath + 'sequence_project.png',
 
-			sort: imageBasePath + 'sort.png',
+		sort: imageBasePath + 'sort.png',
 
-			split: imageBasePath + 'split.png',
+		split: imageBasePath + 'split.png',
 
-			streamAggregate: imageBasePath + 'stream_aggregate.png',
+		streamAggregate: imageBasePath + 'stream_aggregate.png',
 
-			switchStatement: imageBasePath + 'switch.png',
+		switchStatement: imageBasePath + 'switch.png',
 
-			tableValuedFunction: imageBasePath + 'table_valued_function.png',
+		tableValuedFunction: imageBasePath + 'table_valued_function.png',
 
-			tableDelete: imageBasePath + 'table_delete.png',
+		tableDelete: imageBasePath + 'table_delete.png',
 
-			tableInsert: imageBasePath + 'table_insert.png',
+		tableInsert: imageBasePath + 'table_insert.png',
 
-			tableScan: imageBasePath + 'table_scan.png',
+		tableScan: imageBasePath + 'table_scan.png',
 
-			tableSpool: imageBasePath + 'table_spool.png',
+		tableSpool: imageBasePath + 'table_spool.png',
 
-			tableUpdate: imageBasePath + 'table_update.png',
+		tableUpdate: imageBasePath + 'table_update.png',
 
-			tableMerge: imageBasePath + 'table_merge.png',
+		tableMerge: imageBasePath + 'table_merge.png',
 
-			tfp: imageBasePath + 'predict.png',
+		tfp: imageBasePath + 'predict.png',
 
-			top: imageBasePath + 'top.png',
+		top: imageBasePath + 'top.png',
 
-			udx: imageBasePath + 'udx.png',
+		udx: imageBasePath + 'udx.png',
 
-			batchHashTableBuild: imageBasePath + 'batch_hash_table_build.png',
+		batchHashTableBuild: imageBasePath + 'batch_hash_table_build.png',
 
-			windowSpool: imageBasePath + 'table_spool.png',
+		windowSpool: imageBasePath + 'table_spool.png',
 
-			windowAggregate: imageBasePath + 'window_aggregate.png',
+		windowAggregate: imageBasePath + 'window_aggregate.png',
 
-			// cursor operators
-			fetchQuery: imageBasePath + 'fetch_query.png',
+		// cursor operators
+		fetchQuery: imageBasePath + 'fetch_query.png',
 
-			populateQuery: imageBasePath + 'population_query.png',
+		populateQuery: imageBasePath + 'population_query.png',
 
-			refreshQuery: imageBasePath + 'refresh_query.png',
+		refreshQuery: imageBasePath + 'refresh_query.png',
 
-			// shiloh operators
-			result: imageBasePath + 'result.png',
+		// shiloh operators
+		result: imageBasePath + 'result.png',
 
-			aggregate: imageBasePath + 'aggregate.png',
+		aggregate: imageBasePath + 'aggregate.png',
 
-			assign: imageBasePath + 'assign.png',
+		assign: imageBasePath + 'assign.png',
 
-			arithmeticExpression: imageBasePath + 'arithmetic_expression.png',
+		arithmeticExpression: imageBasePath + 'arithmetic_expression.png',
 
-			bookmarkLookup: imageBasePath + 'bookmark_lookup.png',
+		bookmarkLookup: imageBasePath + 'bookmark_lookup.png',
 
-			convert: imageBasePath + 'convert.png',
+		convert: imageBasePath + 'convert.png',
 
-			declare: imageBasePath + 'declare.png',
+		declare: imageBasePath + 'declare.png',
 
-			deleteOperator: imageBasePath + 'delete.png',
+		deleteOperator: imageBasePath + 'delete.png',
 
-			dynamic: imageBasePath + 'dynamic.png',
+		dynamic: imageBasePath + 'dynamic.png',
 
-			hashMatchRoot: imageBasePath + 'hash_match_root.png',
+		hashMatchRoot: imageBasePath + 'hash_match_root.png',
 
-			hashMatchTeam: imageBasePath + 'hash_match_team.png',
+		hashMatchTeam: imageBasePath + 'hash_match_team.png',
 
-			ifOperator: imageBasePath + 'if.png',
+		ifOperator: imageBasePath + 'if.png',
 
-			insert: imageBasePath + 'insert.png',
+		insert: imageBasePath + 'insert.png',
 
-			intrinsic: imageBasePath + 'intrinsic.png',
+		intrinsic: imageBasePath + 'intrinsic.png',
 
-			keyset: imageBasePath + 'keyset.png',
+		keyset: imageBasePath + 'keyset.png',
 
-			locate: imageBasePath + 'locate.png',
+		locate: imageBasePath + 'locate.png',
 
-			populationQuery: imageBasePath + 'population_query.png',
+		populationQuery: imageBasePath + 'population_query.png',
 
-			setFunction: imageBasePath + 'set_function.png',
+		setFunction: imageBasePath + 'set_function.png',
 
-			snapshot: imageBasePath + 'snapshot.png',
+		snapshot: imageBasePath + 'snapshot.png',
 
-			spool: imageBasePath + 'spool.png',
+		spool: imageBasePath + 'spool.png',
 
-			tsql: imageBasePath + 'sql.png',
+		tsql: imageBasePath + 'sql.png',
 
-			update: imageBasePath + 'update.png',
+		update: imageBasePath + 'update.png',
 
-			// fake operators
-			keyLookup: imageBasePath + 'bookmark_lookup.png',
+		// fake operators
+		keyLookup: imageBasePath + 'bookmark_lookup.png',
 
-			// PDW operators
-			apply: imageBasePath + 'apply.png',
+		// PDW operators
+		apply: imageBasePath + 'apply.png',
 
-			broadcast: imageBasePath + 'broadcast.png',
+		broadcast: imageBasePath + 'broadcast.png',
 
-			computeToControlNode: imageBasePath + 'compute_to_control_node.png',
+		computeToControlNode: imageBasePath + 'compute_to_control_node.png',
 
-			constTableGet: imageBasePath + 'const_table_get.png',
+		constTableGet: imageBasePath + 'const_table_get.png',
 
-			controlToComputeNodes: imageBasePath + 'control_to_compute_nodes.png',
+		controlToComputeNodes: imageBasePath + 'control_to_compute_nodes.png',
 
-			externalBroadcast: imageBasePath + 'external_broadcast.png',
+		externalBroadcast: imageBasePath + 'external_broadcast.png',
 
-			externalExport: imageBasePath + 'external_export.png',
+		externalExport: imageBasePath + 'external_export.png',
 
-			externalLocalStreaming: imageBasePath + 'external_local_streaming.png',
+		externalLocalStreaming: imageBasePath + 'external_local_streaming.png',
 
-			externalRoundRobin: imageBasePath + 'external_round_robin.png',
+		externalRoundRobin: imageBasePath + 'external_round_robin.png',
 
-			externalShuffle: imageBasePath + 'external_shuffle.png',
+		externalShuffle: imageBasePath + 'external_shuffle.png',
 
-			get: imageBasePath + 'get.png',
+		get: imageBasePath + 'get.png',
 
-			groupByApply: imageBasePath + 'apply.png',
+		groupByApply: imageBasePath + 'apply.png',
 
-			groupByAggregate: imageBasePath + 'group_by_aggregate.png',
+		groupByAggregate: imageBasePath + 'group_by_aggregate.png',
 
-			join: imageBasePath + 'join.png',
+		join: imageBasePath + 'join.png',
 
-			localCube: imageBasePath + 'intrinsic.png',
+		localCube: imageBasePath + 'intrinsic.png',
 
-			project: imageBasePath + 'project.png',
+		project: imageBasePath + 'project.png',
 
-			shuffle: imageBasePath + 'shuffle.png',
+		shuffle: imageBasePath + 'shuffle.png',
 
-			singleSourceRoundRobin: imageBasePath + 'single_source_round_robin.png',
+		singleSourceRoundRobin: imageBasePath + 'single_source_round_robin.png',
 
-			singleSourceShuffle: imageBasePath + 'single_source_shuffle.png',
+		singleSourceShuffle: imageBasePath + 'single_source_shuffle.png',
 
-			trim: imageBasePath + 'trim.png',
+		trim: imageBasePath + 'trim.png',
 
-			union: imageBasePath + 'union.png',
+		union: imageBasePath + 'union.png',
 
-			unionAll: imageBasePath + 'union_all.png'
-		};
+		unionAll: imageBasePath + 'union_all.png'
+	};
 
-    return iconPaths;
+	return iconPaths;
 }
